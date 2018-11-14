2457c4b1f6594ab09dc03ca249a015f12887ab5f
Aurelien Vallee <aurelien.vallee@axa-im.com>
2018-11-14 23:42:49 +0800

    Template savr.h from configure.ac

--- savr-tests/test_dummy.c
+++ savr-tests/test_dummy.c
@@ -19,8 +19,12 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+#include <savr/savr.h>
+
 #include "test.h"
 
 void test()
 {
+    printf("SAVR API version: %s\n", SAVR_API_VERSION);
+    printf("SAVR ABI version: %s\n", SAVR_ABI_VERSION);
 }
